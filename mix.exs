d2211a7ca93cfd36d6f7afe1a3009aaf0df1a4e7
T. Mjoen <trond@twined.net>
2020-07-04 13:26:14 +0200

    Speed up tests - add coverage

--- mix.exs
+++ mix.exs
@@ -167,7 +167,8 @@ defmodule Brando.Mixfile do
   defp aliases do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"]
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "ecto.seed": ["run priv/repo/seeds.exs"]
     ]
   end
 end
